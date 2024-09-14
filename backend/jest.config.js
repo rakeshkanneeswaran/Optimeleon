12a82e169e15389adaaeb49931a8ec93136b8b81
Rakesh Kanneeswaran <rakikanneeswaran@gmail.com>
2024-09-14 13:00:30 +0530

    changed jest config file

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,14 +1,6 @@
-
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',                       // Use ts-jest for TypeScript support
-    testEnvironment: 'node',                 // Use the Node environment for backend tests
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',              // Transform TypeScript files using ts-jest
-    },
-    testMatch: ['<rootDir>/src/tests/**/*.test.ts'], // Look for test files in src/tests
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-    roots: ["<rootDir>/src/tests"],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],                    // Use the project root directory
-
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ["<rootDir>/src/tests/**/*.ts"]
   };
-  
