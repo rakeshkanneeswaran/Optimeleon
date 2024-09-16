71a3577d0c89076934b8aa38925e1d7c80ae12c8
Rakesh Kanneeswaran <rakikanneeswaran@gmail.com>
2024-09-16 22:16:01 +0530

    added CI/CD pipeline for backend

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -13,7 +13,7 @@ const SignIn: React.FC = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://127.0.0.1:3001/api/user/signin", {
+      const response = await axios.post("http://13.232.196.220/api/user/signin", {
         username,
         password,
       });
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -20,7 +20,7 @@ const SignUp: React.FC = () => {
       return;
     }
     try {
-      const response = await axios.post("http://127.0.0.1:3001/api/user/signup", {
+      const response = await axios.post("http://13.232.196.220/api/user/signup", {
         username,
         name,
         password,
