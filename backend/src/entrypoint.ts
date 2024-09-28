12db0421ebebe7ef909bc714a7e61f195f26b3dc
rakeshkanneeswaran <rakikanneeswaran@gmail.com>
2024-09-28 11:10:31 +0530

    vertically scaled the application

--- backend/src/entrypoint.ts
+++ backend/src/entrypoint.ts
@@ -1,4 +1,25 @@
 import app from ".";
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-});
+import process from "node:process";
+import { availableParallelism } from "node:os"
+import cluster from "node:cluster"
+
+const numOfCpus = availableParallelism();
+
+if (cluster.isPrimary) {
+
+    console.log(`Master ${process.pid} is running`);
+    for (let i = 0; i < numOfCpus; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', () => {
+        console.log(`Worker ${process.pid} died`);
+        cluster.fork();
+    })
+}
+else {
+    app.listen(3001, () => {
+        console.log('Server is running on port 3001');
+    });
+}
+
+
